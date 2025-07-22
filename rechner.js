aa2a0bdf8f323171ea2f9e6966636c067be22dce
Kevin Hase <hasekevin21@googlemail.com>
2025-07-22 10:36:49 +0200

    changed css, burgermenu and responsiv

--- rechner.js
+++ rechner.js
@@ -25,9 +25,10 @@ window.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
     const burger = document.getElementById("burger");
-    const nav = document.querySelector("nav.links");
+    const links = document.querySelector(".links");
 
     burger.addEventListener("click", () => {
-      nav.classList.toggle("open");
+      links.classList.toggle("open");
+      burger.classList.toggle("active");
     });
   });
